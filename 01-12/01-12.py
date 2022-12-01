4fc5c3e8146ca64467a7010afa862a0867956595
Samay Sagar <manu.sagarg1@gmail.com>
2022-12-01 23:17:31 +0800

    Comments

--- 01-12/01-12.py
+++ 01-12/01-12.py
@@ -1,10 +1,12 @@
-
+# Another approach will be to just keep track of all the sums and then find the max (for part 1)
+# and top three (for part 2)
 
 def part1():
     val = 0
     with open('input.txt') as f:
         sum = 0
         for line in f:
+            # Neat way to check if line is null
             if len(line.strip()) == 0 :
                 val = max(sum,val)
                 sum = 0
@@ -19,6 +21,7 @@ def part2():
     with open('input.txt') as f:
         sum = 0
         for line in f:
+            # There must be a faster way for this
             if len(line.strip()) == 0 :
                 if sum >= val1:
                     val3 = val2
